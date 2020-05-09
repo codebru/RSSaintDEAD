7882d85c4c30bfc9a1c0f16d3d8f1505c8cb40bb
Philip Bruland <philipbruland@gmail.com>
2020-05-09 13:38:55 +0100

    Move feed get to the home page

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,35 +1,12 @@
-import React, { useState } from 'react';
-import {
-  useIonViewWillEnter,
-  useIonViewWillLeave,
-} from '@ionic/react';
+import React from 'react';
 import {
   Card,
   Spin,
   Space,
 } from 'antd';
-import Parser from 'rss-parser';
-const parser = new Parser();
 
-// Note: some RSS feeds can't be loaded in the browser due to CORS security.
-// To get around this, you can use a proxy.
-const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-const FEED = 'http://feeds.bbci.co.uk/news/world/africa/rss.xml';
-
-async function getFeed(feedURL) {
-  let feed = await parser.parseURL(`${CORS_PROXY}${feedURL}`);
-  console.log(feed.items[0])
-  return feed;
-}
-
-function Feed() {
-  const [feed, setFeed] = useState(null);
-  useIonViewWillEnter(() => {
-    async function data() {
-      setFeed(await getFeed(FEED))
-    }
-    data();
-  })
+function Feed(props) {
+  const { feed } = props;
   return (
     <Space direction="vertical" style={{ width: '100%', paddingLeft: '0.5rem', paddingRight: '0.5rem' }}>
       {feed
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,34 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  useIonViewWillEnter,
+  useIonViewWillLeave,
+} from '@ionic/react';
+import Parser from 'rss-parser';
 import Feed from '../../components/Feed';
 import './Style.css';
 
+const parser = new Parser();
+// Note: some RSS feeds can't be loaded in the browser due to CORS security.
+// To get around this, you can use a proxy.
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+const FEED = 'http://feeds.bbci.co.uk/news/world/africa/rss.xml';
+
+async function getFeed(feedURL) {
+  let feed = await parser.parseURL(`${CORS_PROXY}${feedURL}`);
+  console.log(feed.items[0])
+  return feed;
+}
+
 const Home = () => {
+  const [feed, setFeed] = useState(null);
+  useIonViewWillEnter(() => {
+    async function data() {
+      setFeed(await getFeed(FEED))
+    }
+    data();
+  })
+ 
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +42,7 @@ const Home = () => {
             <IonTitle size="large">BBC Africa</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <Feed />
+        <Feed feed={feed} />
       </IonContent>
     </IonPage>
   );
